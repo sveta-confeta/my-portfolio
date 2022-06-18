e8eaea7c30a9a17cfd90f0c855996fb25f2e0f65
sveta-confeta <_sveta_@tut.by>
2022-06-18 22:21:21 +0300

    сÑIcons

--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './Skills.module.css';
 
+
 export const Skill = (props) => {
     return (
 
         <div className={s.card}>
             <div className={`${s.face} ${s.face1}`} >
                 <div className="content">
-                    <img
-                        src="https://www.flaticon.com/ru/free-icon/css-3_732190?term=css&page=1&position=1&page=1&position=1&related_id=732190&origin=search#"/>
+                    <div className={s.icon} style={props.style}>
+                    </div>
                         <h3> {props.title}</h3>
                 </div>
             </div>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,8 +2,45 @@ import React from 'react';
 import {Skill} from "./Skill";
 import s from './Skills.module.css'
 import {Title} from "../ForAll/Title/Title";
+import react from './../assets/image/icons/atom.png'
+import js from './../assets/image/icons/js.png'
+import css from './../assets/image/icons/css.png'
+import git from './../assets/image/icons/git.png'
+import api from './../assets/image/icons/api.png'
+import web from './../assets/image/icons/web.png'
+import photoSh from './../assets/image/icons/photoshop.png'
+import figma from './../assets/image/icons/figma.png'
+import ts from './../assets/image/icons/typescript.png'
+
 
 export const Skills = () => {
+    const reactIcon ={
+        backgroundImage:`url(${react})`
+    }
+    const jsIcon ={
+        backgroundImage:`url(${js})`
+    }
+    const cssIcon ={
+        backgroundImage:`url(${css})`
+    }
+    const gitIcon ={
+        backgroundImage:`url(${git})`
+    }
+    const apiIcon ={
+        backgroundImage:`url(${api})`
+    }
+    const webIcon ={
+        backgroundImage:`url(${web})`
+    }
+    const photoIcon ={
+        backgroundImage:`url(${photoSh})`
+    }
+    const figmaIcon ={
+        backgroundImage:`url(${figma})`
+    }
+    const tsIcon ={
+        backgroundImage:`url(${ts})`
+    }
     return (
         <div className={s.skillsWrapper}>
 
@@ -11,15 +48,15 @@ export const Skills = () => {
             <div className={s.wrapper}>
                <Title title={'my skills'}/>
             <div className= {s.skills} >
-                <Skill title={'JS'} description={'описание навыка JS'}/>
-                <Skill title={'React'} description={'описание навыка React'}/>
-                <Skill title={'CSS'} description={'описание навыка Css'}/>
-                <Skill title={'GIT'} description={'описание навыка Css'}/>
-                <Skill title={'REST API'} description={'описание навыка Css'}/>
-                <Skill title={'WEB-Design'} description={'описание навыка Css'}/>
-                <Skill title={'Photoshop'} description={'описание навыка Css'}/>
-                <Skill title={'Figma'} description={'описание навыка Css'}/>
-                <Skill title={'TypeScript'} description={'описание навыка Css'}/>
+                <Skill title={'JS'} description={'описание навыка JS'} style={reactIcon}/>
+                <Skill title={'React'} description={'описание навыка React'} style={jsIcon}/>
+                <Skill title={'CSS'} description={'описание навыка Css'} style={cssIcon}/>
+                <Skill title={'GIT'} description={'описание навыка Css'} style={gitIcon}/>
+                <Skill title={'REST API'} description={'описание навыка Css'} style={apiIcon}/>
+                <Skill title={'WEB-Design'} description={'описание навыка Css'} style={webIcon}/>
+                <Skill title={'Photoshop'} description={'описание навыка Css'}  style={photoIcon}/>
+                <Skill title={'Figma'} description={'описание навыка Css'} style={figmaIcon}/>
+                <Skill title={'TypeScript'} description={'описание навыка Css'} style={tsIcon}/>
                 <Skill title={'Redux'} description={'описание навыка Css'}/>
                 <Skill title={'Bootstrap'} description={'описание навыка Css'}/>
                 <Skill title={'Material UI'} description={'описание навыка Css'}/>

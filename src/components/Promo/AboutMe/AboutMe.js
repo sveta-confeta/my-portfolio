81d0fbe578fcca29b1c48edc2a0105a7670a143e
sveta-confeta <_sveta_@tut.by>
2022-07-31 23:20:17 +0300

    media promo

--- src/components/Promo/AboutMe/AboutMe.js
+++ src/components/Promo/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
                 <div className={s.img}>
                 </div>
                 <div className={s.title}><div className={s.sectionTitle}> A STORY ABOUT ME </div></div>
-                <p className={s.aboutP}>I'm Svetlana Shunkova and I'm a responsible and motivated developer with
+                <p className={s.aboutP}>I'm a responsible and motivated developer with
                     experience in creating SPA, JS (Typescript), React, Redux,
                     leading-page using CSS and SASS. Now I am improving my
                     skills in this direction and expanding them with the help of

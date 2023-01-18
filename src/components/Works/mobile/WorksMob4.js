ce6dfee5a96197f67d52c0711753db96ec149121
sveta-confeta <_sveta_@tut.by>
2023-01-18 10:58:36 +0300

    add notes

--- src/components/Works/mobile/WorksMob4.js
+++ src/components/Works/mobile/WorksMob4.js
@@ -4,16 +4,24 @@ import s from './../Works.module.scss'
 import {Title} from "../../Common/Title/Title";
 import {SellerModal} from "../modal/SellerModal";
 import sellerImg from "./../../assets/image/seller.jpg";
+import notesImage from "../../assets/image/notes.jpg";
+import {NotesModal} from "../modal/NotesModal";
 
 export const WorksMob4 = () => {
 
     const [openModalSeller,setOpenModalSeller]=useState(false)
+    const [openModalNotes,setOpenModalNotes]=useState(false)
 
 
     const seller={
         backgroundImage:`url(${sellerImg})`,
         backgroundSize:'cover',
     }
+    const notes={
+        backgroundImage:`url(${notesImage})`,
+        backgroundSize:'cover',
+    }
+
 
     return (
 
@@ -22,10 +30,12 @@ export const WorksMob4 = () => {
          <Title title={'my project'}/>
             <div className={s.wrapper}>
                 <Work setOpenModal={setOpenModalSeller} style={seller} title={'Sellers-hab'}  description={'landing page assembled on the React'}/>
+                <Work setOpenModal={setOpenModalNotes} style={notes} title={'Simple Notes'}  description={'Team project : a site-application of notes'}/>
 
 
             </div>
             <SellerModal isOpenModal={openModalSeller} setIsOpenModal={setOpenModalSeller}/>
+            <NotesModal isOpenModal={openModalNotes} setIsOpenModal={setOpenModalNotes}/>
 
         </div>
     );
